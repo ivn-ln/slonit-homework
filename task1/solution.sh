f016a88dacbf1dff16d2a0135864daeb8a494860
ivn-ln <illarn@ya.ru>
2024-11-09 15:47:51 +0300

    Homework 1 done

--- task1/solution.sh
+++ task1/solution.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-a=$(ip a | grep -P "inet.*global dynamic")
-echo Active ipv4 interface: $a
+ip=$(ip a | grep -P "inet.*global dynamic")
+echo Active ipv4 interface: $ip
 mkfifo pipe
 ss -plnt > pipe &
 cat pipe > sockets.txt
 rm pipe
-echo File written
+echo File sockets.txt written
+mkfifo log
+sudo cat /var/log/boot.log > log &
+cat log | gzip -f > log.gz 
+rm log
+echo Logs archived in log.gz
+mkfifo format
+echo %d/%m 20%y > format &
+echo %d/%m 20%y | (read f; echo Current date: $(date +"$f");)
+rm format
 cat << EOF > file.txt
 Lorem
 ipsum
 EOF
+echo File file.txt written
